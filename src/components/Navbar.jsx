fdec7615159857cd41bce02577c6d7e3059b4f06
lucas-possato-dev <lucaspstinho@gmail.com>
2023-04-20 02:36:47 -0300

    complete

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+
+const Navbar = () => {
+
+  const [nav, setNav] = React.useState(true);
+
+  const handleNav = () => {
+    setNav(!nav);
+  }
+
+  return (
+    <div className=' flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+      <ul className='hidden md:flex'>
+        <li className='p-4 cursor-pointer hover:text-gray-400'>Home</li>
+        <li className='p-4 cursor-pointer hover:text-gray-400'>Company</li>
+        <li className='p-4 cursor-pointer hover:text-gray-400'>Resources</li>
+        <li className='p-4 cursor-pointer hover:text-gray-400'>About</li>
+        <li className='p-4 cursor-pointer hover:text-gray-400'>Contact</li>
+      </ul>
+      <div onClick={handleNav} className='block md:hidden'>
+        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
+      <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900  bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+          <ul className='uppercase p-4'>
+          <li className='p-4 border-b border-gray-600 cursor-pointer hover:bg-white hover:text-black transition-all duration-300'>Home</li>
+          <li className='p-4 border-b border-gray-600 cursor-pointer hover:bg-white hover:text-black transition-all duration-300'>Company</li>
+          <li className='p-4 border-b border-gray-600 cursor-pointer hover:bg-white hover:text-black transition-all duration-300'>Resources</li>
+          <li className='p-4 border-b border-gray-600 cursor-pointer hover:bg-white hover:text-black transition-all duration-300'>About</li>
+          <li className='p-4 border-b border-gray-600 cursor-pointer hover:bg-white hover:text-black transition-all duration-300'>Contact</li>
+          </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
